8487e5c59f040812293a41708f010dfc0044b2cf
RinnahMekonnen <rinnah.mekonnen@bruins.belmont.edu>
2022-11-02 18:17:21 -0500

    Created unit tests to addNegativeBalance,ZeroTest

--- tests/VendorTest.java
+++ tests/VendorTest.java
@@ -6,7 +6,27 @@ public class VendorTest {
 
     @Test
     void addition() {
-        assertEquals(2, 1 + 1);
+        Vendor v = new Vendor(2,5);
+        v.addMoney(6);
+        assertEquals(6, v.getBalance());
+    }
+    @Test
+    void addZeroTest() {
+        Vendor v = new Vendor(2,5);
+        v.addMoney(0);
+        assertEquals(0, v.getBalance());
+    }
+    @Test
+    void addNegativeBalance() {
+        Vendor v = new Vendor(2,5);
+        v.addMoney(-10);
+        assertEquals(0, v.getBalance());
     }
 
+    @Test
+    void addNegativeTestInitialBalancePositive() {
+        Vendor v = new Vendor(2, 5);
+        v.addMoney(10);
+        v.addMoney(-5);
+    }
 }
